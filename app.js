9d583effb6fe14161dd528af256a28667e14c2d6
khjhs101198 <khjhs101198@gmail.com>
2021-02-16 00:15:51 +0800

    Add /img get

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ const jsonParser = express.json();
 
 app.set("view engine", "ejs");
 app.use(urlencoded, jsonParser);
-app.use(function(req, res, next){
-  if(req.headers.cookie) {
-    next();
-  }
-  else {
-    res.status(404).send("The request doesn't have cookie");
-  }
-}, express.static("./public"));
+app.use(express.static("./public"));
 
 app.get("/", function(req, res){
   if(!req.headers.cookie) {
     res.set("Set-Cookie", ["name=Jimmy; SameSite=Lax"]);
   }
   res.render("main");
+});
+
+app.get("/img", function(req, res){
+  if(req.headers.cookie) {
+    res.sendFile(__dirname+"/public/images/w18.jpg");
+  }
+  else {
+    res.send("No cookie");
+  }
 });
 
 app.listen(process.env.PORT||3000, function(err){
