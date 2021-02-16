b6569d499c14ac1dbc122ec7576814a52c1d8b2e
khjhs101198 <khjhs101198@gmail.com>
2021-02-17 00:25:02 +0800

    Server as a ad website

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
 $("#pc").on("click", function(){
   if(document.cookie.indexOf("pcView")!==-1) {
     let cur = Number( document.cookie.slice(document.cookie.indexOf("pcS")+3, document.cookie.indexOf("pcE") ) );
-    document.cookie = `pcView=pcS${cur+1}pcE; SameSite=Lax`;
+    document.cookie = `pcView=pcS${cur+1}pcE; SameSite=None`;
   }
   else {
-    document.cookie = "pcView=pcS0pcE; SameSite=Lax";
+    document.cookie = "pcView=pcS0pcE; SameSite=None";
   }
 });
