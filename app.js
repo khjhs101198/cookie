4303882d3d6370c176b564afac68a5d8e57c380f
khjhs101198 <khjhs101198@gmail.com>
2021-02-19 22:46:11 +0800

    Modify maxAge value

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ app.set("view engine", "ejs");
 app.use(urlencoded, jsonParser, cookieParser());
 app.use(express.static("./public"));
 
-function addCookie(req, res, next){
-  if(req.cookies.viewHis===undefined) {
-    res.cookie("viewHis", "ct0", {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 86400});
-  }
-  next();
-}
-
 app.get("/", function(req, res){
   res.render("main");
 });
@@ -32,16 +25,16 @@ app.get("/cookies/outerInf", function(req, res){
 
 app.put("/cookies/modifyCookie", function(req, res) {
   if(req.cookies.viewHis===undefined) {
-    res.cookie("viewHis", `ct1${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 86400});
+    res.cookie("viewHis", `ct1${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 115200000});
   }
   else {
     let cc = req.cookies.viewHis;
     let curCount = Number( cc.match(`(?<=ct)[0-9]*`) );
     if(curCount<10) {
-      res.cookie("viewHis", `${cc.replace( new RegExp("(?<=ct)[0-9]*"), curCount+1)}${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 86400});
+      res.cookie("viewHis", `${cc.replace( new RegExp("(?<=ct)[0-9]*"), curCount+1)}${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 115200000});
     }
     else {
-      res.cookie("viewHis", `${cc.replace( new RegExp("ct10[A-Z,a-z]*1"), "ct10")}${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 86400});
+      res.cookie("viewHis", `${cc.replace( new RegExp("ct10[A-Z,a-z]*1"), "ct10")}${req.body.type}1`, {sameSite: "None" ,secure: true, path: "/cookies", httpOnly: true, maxAge: 115200000});
     }
   }
   res.send("Modify cookies successfully!");
